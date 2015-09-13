ec378c6e8533f27c3f1683c7b0f5274760b3db9f
alexiscaspell <alexiscaspell@gmail.com>
2015-09-13 15:51:31 -0300

    Subo script para usar commonsDeAsedio sin scripts

---
+++ commonsDeAsedio/installCommonsDeAsedio.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# -*- ENCODING: UTF-8 -*-
+echo "LD_LIBRARY_PATH=/home/utnso/git/tp-2015-2c-los-javimancos/commonsDeAsedio/Debug" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH" >> ~/.bashrc
+exit
