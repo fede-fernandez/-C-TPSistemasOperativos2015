f0c9db3c38bc2e8ffc9108a0f5b8d418cde85363
Federico Fernandez <fede.a25@gmail.com>
2015-09-14 20:48:27 -0300

    Funcion principal del CPU desarrollada, funciones de cada instruccion de un programa realizadas parcialmente con comentarios por falta de estructuras/sockets

--- CPU/src/funcionesCPU.c
+++ CPU/src/funcionesCPU.c
@@ -41,3 +41,70 @@ tipoConfigCPU* cargarArchivoDeConfiguracionDeCPU(char* rutaDelArchivoDeConfigura
 
 	return cfg;
 }
+
+ejecutarInstruccion(char* instruccion, int idDeProceso)
+{
+	char** instruccionSeparadaPorEspacios = string_split(instruccion, " ");
+
+	if(string_equals_ignore_case(instruccionSeparadaPorEspacios[0], "iniciar"))
+		instruccionIniciar(instruccionSeparadaPorEspacios[1], idDeProceso);
+
+	if(string_equals_ignore_case(instruccionSeparadaPorEspacios[0], "leer"))
+		instruccionLeer(instruccionSeparadaPorEspacios[1], idDeProceso);
+
+	if(string_equals_ignore_case(instruccionSeparadaPorEspacios[0], "escribir"))
+		instruccionEscribir(instruccionSeparadaPorEspacios[1],
+				instruccionSeparadaPorEspacios[2], idDeProceso);
+
+	if(string_equals_ignore_case(instruccionSeparadaPorEspacios[0], "entrada-salida"))
+		instruccionEntradaSalida(instruccionSeparadaPorEspacios[1], idDeProceso);
+
+	if(string_equals_ignore_case(instruccionSeparadaPorEspacios[0], "finalizar"))
+		instruccionFinalizar(idDeProceso);
+}
+
+//Por ahora envio a Memoria solo idDelProceso para que lo asigne a la pagina, si necesita
+//mas cosas avisenme, o usamos el pcb para todo.
+
+int instruccionIniciar(int numeroDePaginas, int idDeProceso)
+{
+	//Envio socket a Proceso Memoria con la instrucción iniciar
+	//y el número de páginas que voy a pedirle
+	//Proceso Memoria me avisa si se pudo hacer o no
+	return FALSE;
+}
+
+int instruccionLeer(int numeroDePagina)
+{
+	//Envio socket a Proceso Memoria con la instruccion leer
+	//y el numero de pagina que quiero recibir
+	//Proceso memoria me devuelve el resultado
+	//Imprimo el resultado
+	return 0;
+}
+
+int instruccionEscribir(int numeroDePagina, char* textoAEscribir, int idDeProceso)
+{
+	//Envio socket a Proceso Memoria con la instruccion escribir,
+	//el número de página donde quiero escribir y el texto
+	//a escribir, MEMORIA DEBE REEMPLAZAR TODO EL CONTENIDO
+	//SI ESA PAGINA TIENE ALGO
+	//Proceso Memoria me devuelve el resultado
+	//Imprimo el resultado
+	return 0;
+}
+
+int instruccionEntradaSalida(int tiempoDeEspera, int idDeProceso)
+{
+	//Envio socket a Proceso Planificador con el pcb, cambiando
+	//el estado a BLOQUEADO y el tiempo que voy a estarlo
+	//retorno 1 porque tengo que usar break en el while del lector de instrucciones
+	return 1;
+}
+
+int instruccionFinalizar(int idDeProceso)
+{
+	//Envio socket a Proceso Memoria para que libere el
+	//espacio de memoria ocupado por el proceso, también memoria le avisa a Swap.
+	return 1;
+}
