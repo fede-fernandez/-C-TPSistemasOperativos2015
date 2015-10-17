a218ae769cee4b5d35e1bb201cd386e99a9eb571
leoaristarco <chucky_92@hotmail.com.ar>
2015-10-17 16:53:31 -0300

    planificador (con los cambios del laboratorio)

--- Planificador/Debug/src/funcionesPlanificador.d
+++ Planificador/Debug/src/funcionesPlanificador.d
@@ -1,10 +1,4 @@
 src/funcionesPlanificador.d: ../src/funcionesPlanificador.c \
- ../src/funcionesPlanificador.h \
- /home/alexis/git/tp-2015-2c-los-javimancos/commonsDeAsedio/commonsDeAsedio/error.h \
- /home/alexis/git/tp-2015-2c-los-javimancos/commonsDeAsedio/commonsDeAsedio/cliente-servidor.h
+ ../src/funcionesPlanificador.h
 
 ../src/funcionesPlanificador.h:
-
-/home/alexis/git/tp-2015-2c-los-javimancos/commonsDeAsedio/commonsDeAsedio/error.h:
-
-/home/alexis/git/tp-2015-2c-los-javimancos/commonsDeAsedio/commonsDeAsedio/cliente-servidor.h:
