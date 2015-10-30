81cf3b920ed2c46ba0a49c4ce0f70aa1e04b8e38
Ronan Camargo <ronan.cam@gmail.com>
2015-10-30 14:13:29 -0300

    Prueba de conexion

--- SWAP/src/SWAP.c
+++ SWAP/src/SWAP.c
@@ -19,16 +19,16 @@
 #define INFINITO 1
 
 
-tipoInstruccion* crearInstruccion(char ins,int pid,int pag,char* testo){
-	tipoInstruccion* aux = malloc(sizeof(tipoInstruccion));
-
-	aux->instruccion = ins;
-	aux->pid = pid;
-	aux->nroPagina = pag;
-	aux->texto = strdup(testo);
-
-	return aux;
-}
+//tipoInstruccion* crearInstruccion(char ins,int pid,int pag,char* testo){
+//	tipoInstruccion* aux = malloc(sizeof(tipoInstruccion));
+//
+//	aux->instruccion = ins;
+//	aux->pid = pid;
+//	aux->nroPagina = pag;
+//	aux->texto = strdup(testo);
+//
+//	return aux;
+//}
 
 int main(void) {
 
@@ -45,29 +45,35 @@ int main(void) {
 /*************Sockets***************/
 	int socketParaAdministrador = crearSocket();
 	asociarAPuerto(socketParaAdministrador,configuracion->puertoDeEscucha);
+
+	printf("Escuchando conexiones...\n");
 	escucharConexiones(socketParaAdministrador,MAXIMAS_CONEXIONES_ENTRANTES);
 	int socketParaRecibirInstrucciones = crearSocketParaAceptarSolicitudes(socketParaAdministrador);
+	printf("Conexion establecida\n");
 
 
 
 /***********Recibir instrucciones y ejecutarlas***********/
-	while(INFINITO){
-
-		instruccionAEjecutar = recibirInstruccion(socketParaRecibirInstrucciones);
-
-		respuestaParaMemoria = ejecutarInstruccion(instruccionAEjecutar,listaDeHuecosUtilizados,configuracion);
-
-		enviarRespuesta(socketParaRecibirInstrucciones,respuestaParaMemoria);
-
-		destruirTipoRespuesta(respuestaParaMemoria);
-		destruirTipoInstruccion(instruccionAEjecutar);
-	}
+//	while(INFINITO){
+//
+//		instruccionAEjecutar = recibirInstruccion(socketParaRecibirInstrucciones);
+//
+//		respuestaParaMemoria = ejecutarInstruccion(instruccionAEjecutar,listaDeHuecosUtilizados,configuracion);
+//
+//		enviarRespuesta(socketParaRecibirInstrucciones,respuestaParaMemoria);
+//
+//		destruirTipoRespuesta(respuestaParaMemoria);
+//		destruirTipoInstruccion(instruccionAEjecutar);
+//	}
 
 /********Liberar memoria*************/
 
+
+	printf("Libero sockets\n");
 	liberarSocket(socketParaRecibirInstrucciones);
 	liberarSocket(socketParaAdministrador);
 
+	printf("Libero estructuras\n");
 	list_destroy_and_destroy_elements(listaDeHuecosUtilizados,(void*)destruirHuecoUtilizado);
 	destruirConfigSWAP(configuracion);
 
