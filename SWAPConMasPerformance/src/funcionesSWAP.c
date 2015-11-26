c32ca4e853e6c3e6e6ca346b1c7e84d4d55d95df
Ronan Camargo <ronan.cam@gmail.com>
2015-11-26 15:29:05 -0300

    Subo un nuevo SWAP con mas performance

---
+++ SWAPConMasPerformance/src/funcionesSWAP.c
@@ -0,0 +1,62 @@
+/*
+ * funcionesSWAP.c
+ *
+ *  Created on: 4/9/2015
+ *      Author: utnso
+ */
+
+#include "funcionesSWAP.h"
+
+////////////////////FUNCIONES PARA EL MANEJO DE ARCHIVO DE CONFIGURACION///////////////////////
+
+tipoConfigSWAP* crearConfigSWAP(){
+	tipoConfigSWAP* cfg = malloc(sizeof(tipoConfigSWAP));
+	cfg->nombreDeSWAP = string_new();
+
+	return cfg;
+}
+
+void destruirConfigSWAP (tipoConfigSWAP* cfg){
+	free(cfg->nombreDeSWAP);
+	free(cfg);
+}
+
+tipoConfigSWAP* cargarArchivoDeConfiguracionDeSWAP(char* rutaDelArchivoDeConfiguracion){
+
+	t_config* archivoCfg = config_create(rutaDelArchivoDeConfiguracion);
+	validarExistenciaDeArchivoDeConfiguracion(rutaDelArchivoDeConfiguracion);
+
+	tipoConfigSWAP* cfg = crearConfigSWAP();
+
+	validarErrorYAbortar(config_has_property(archivoCfg,PUERTO_ESCUCHA)
+			&& config_has_property(archivoCfg,NOMBRE_SWAP)
+			&& config_has_property(archivoCfg,CANTIDAD_PAGINAS)
+			&& config_has_property(archivoCfg,TAMANIO_PAGINA)
+			&& config_has_property(archivoCfg,RETARDO_COMPACTACION)
+			&& config_has_property(archivoCfg,RETARDO_SWAP),
+			"Las claves del archivo de configuracion no coinciden con las que requiere el SWAP");
+
+
+	cfg->puertoDeEscucha = config_get_int_value(archivoCfg,PUERTO_ESCUCHA);
+	cfg->nombreDeSWAP = string_duplicate(config_get_string_value(archivoCfg,NOMBRE_SWAP));
+	cfg->cantidadDePaginas = config_get_int_value(archivoCfg,CANTIDAD_PAGINAS);
+	cfg->retardoDeCompactacion = config_get_int_value(archivoCfg,RETARDO_COMPACTACION);
+	cfg->tamanioDePagina = config_get_int_value(archivoCfg,TAMANIO_PAGINA);
+	cfg->retardoDeSWAP = config_get_int_value(archivoCfg,RETARDO_SWAP);
+
+
+	config_destroy(archivoCfg);
+
+	return cfg;
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ SWAPConMasPerformance/src/particionSWAP.c
@@ -0,0 +1,116 @@
+/*
+ * particionSWAP.c
+ *
+ *  Created on: 14/9/2015
+ *      Author: utnso
+ */
+
+
+#include "particionSWAP.h"
+
+
+char* recuperarBloque(char* bloqueAVaciar);
+char* completarBloque(char* bloqueACompletar, int tamanioDeBloque);
+void borrarBloque(char* rutaDeParticion, int numeroDeBloque, int tamanioDeBloque);
+
+
+
+//////////////FUNCIONES PARA EL ARCHIVO DE PARTICION///////////////////////
+
+
+void inicializarParticion(char* nombreDeParticion,int tamanioDePagina,int cantidadDePaginas){
+
+	//FILE* particion;
+
+	char* instruccion = string_new();
+	string_append_with_format(&instruccion,"dd if=/dev/zero of=%s bs=%d count=%d",nombreDeParticion,tamanioDePagina,cantidadDePaginas);
+
+	printf("Inicializando particion...\n\n");
+	system(instruccion);
+	printf("Particion inicializada. \n");
+
+	free(instruccion);
+
+	//particion = fopen(nombreDeParticion,"r+");//modo actualizacion, el archivo debe existir
+	//return particion;
+}
+
+
+
+
+void escribirBloqueMapeado(char* rutaDeParticion,char* contenidoAEscribir,int numDeBloque, int tamanioDeBloque){
+
+	FILE* archivo = fopen(rutaDeParticion,"r+");
+
+	fseek(archivo,numDeBloque*tamanioDeBloque,SEEK_SET);
+
+	char* aux = completarBloque(contenidoAEscribir,tamanioDeBloque);
+
+	fwrite(aux,tamanioDeBloque,1,archivo);
+
+	//free(aux);
+
+	fclose(archivo);
+}
+
+char* leerBloqueMapeado(char* rutaDeParticion,int numDeBloque, int tamanioDeBloque){
+	FILE* archivo = fopen(rutaDeParticion,"r+");
+
+	char* leido = string_new();
+
+	fseek(archivo,numDeBloque*tamanioDeBloque,SEEK_SET);
+
+	fread(leido,tamanioDeBloque,1,archivo);
+
+	char* aux = recuperarBloque(leido);
+
+	fclose(archivo);
+	free(leido);
+	//free(aux);
+
+	return aux;
+}
+
+
+void borrarMProcDeParticion(char* rutaDeParticion,int baseDeMProc,int cantidadDePaginasDeMProc,int tamanioDeBloque){
+
+	int i;
+	for (i = 0; i < cantidadDePaginasDeMProc; ++i) {
+
+		borrarBloque(rutaDeParticion,baseDeMProc,tamanioDeBloque);
+		baseDeMProc++;
+	}
+}
+
+
+/////////AUXILIARES/////////
+
+
+char* completarBloque(char* bloqueACompletar, int tamanioDeBloque){
+
+	int cantACompletar = tamanioDeBloque - string_length(bloqueACompletar);
+	char* aux = string_duplicate(bloqueACompletar);
+
+	string_append(&aux,string_repeat(CARACTER_VACIO,cantACompletar));
+
+	return aux;
+}
+
+char* recuperarBloque(char* bloqueAVaciar){
+	char* bloque = string_new();
+	int i;
+
+	for (i = 0; i < string_length(bloqueAVaciar); ++i) {
+		if (bloqueAVaciar[i] == CARACTER_VACIO)
+			break;
+	}
+
+	bloque = string_substring_until(bloqueAVaciar,i);
+
+	return bloque;
+}
+
+void borrarBloque(char* rutaDeParticion, int numeroDeBloque, int tamanioDeBloque){
+	escribirBloqueMapeado(rutaDeParticion,"",numeroDeBloque,tamanioDeBloque);
+}
+
