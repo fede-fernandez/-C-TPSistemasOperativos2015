001a7a3c8fcf95838f8a38b7375276c72fded8fa
Federico Fernandez <fede.a25@gmail.com>
2015-12-03 02:53:32 -0300

    Colores en commons

--- CPU/src/funcionesCPU.h
+++ CPU/src/funcionesCPU.h
@@ -102,19 +102,6 @@ typedef struct respuestaDeInstruccion{
 }tipoRepuestaDeInstruccion;
 /**********Fin de Estructuras del CPU**********/
 
-/**********Colores**********/
-#define NEGRO "\033[1m\033[30m"
-#define ROJO "\033[1m\033[31m"
-#define VERDE "\033[1m\033[32m"
-#define AMARILLO "\033[1m\033[33m"
-#define AZUL "\033[1m\033[34m"
-#define ROSA "\033[1m\033[35m"
-#define CELESTE "\033[1m\033[36m"
-#define BLANCO "\033[1m\033[37m"
-#define FINDETEXTO "\033[0m"
-/*********Fin de Colores**********/
-
-
 /**********Funciones del CPU**********/
 /*Hilo CPU, se conecta con Planificador y Memoria, espera PCBs y los ejecuta*/
 void* unCPU(t_datosCPU*);
